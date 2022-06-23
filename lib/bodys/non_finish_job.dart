f4d42af203558585ded8babe2e5dcf681410bec4
userkyi <thitiwat0smart@gmail.com>
2022-06-23 12:05:52 +0700

    Before Lunch 23-6-65

--- lib/bodys/non_finish_job.dart
+++ lib/bodys/non_finish_job.dart
@@ -1,8 +1,13 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, avoid_print, unused_local_variable
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:ungegat/models/job_model.dart';
 import 'package:ungegat/utility/my_constant.dart';
+import 'package:ungegat/widgets/show_progess.dart';
 
 import 'package:ungegat/widgets/show_text.dart';
 
@@ -19,6 +24,7 @@ class NonFinishJob extends StatefulWidget {
 
 class _NonFinishJobState extends State<NonFinishJob> {
   var dataUserLogin = <String>[];
+  var jobModels = <JobModel>[];
 
   @override
   void initState() {
@@ -28,7 +34,25 @@ class _NonFinishJobState extends State<NonFinishJob> {
   }
 
   Future<void> readDatajob() async {
-    String path = 'https://www.androidthai.in.th/egat/getUserWhereidOfficeKYI.php?isAdd=true&idOfficer=$idOffecer';
+    String idOfficer = dataUserLogin[0];
+    String path =
+        'https://www.androidthai.in.th/egat/getUserWhereidOfficeKYI.php?isAdd=true&idOfficer=$idOfficer';
+
+    await Dio().get(path).then((value) {
+      print('value ===>> $value');
+
+      var result = json.decode(value.data);
+      for (var element in result) {
+        JobModel jobModel = JobModel.fromMap(element);
+        print('job ===> ${jobModel.job}');
+
+        if (jobModel.status == 'Start') {
+          jobModels.add(jobModel);
+        }
+      }
+
+      setState(() {});
+    });
   }
 
   @override
@@ -36,7 +60,16 @@ class _NonFinishJobState extends State<NonFinishJob> {
     return Column(
       children: [
         showTitle(head: 'ชื่อพนักงาน :', value: dataUserLogin[1]),
-        showTitle(head: 'ตำแหน่ง :', value: dataUserLogin[2])
+        showTitle(head: 'ตำแหน่ง :', value: dataUserLogin[2]),
+        jobModels.isEmpty
+            ? const ShowProgess()
+            : ListView.builder(
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+                itemCount: jobModels.length,
+                itemBuilder: (context, index) =>
+                    ShowText(text: jobModels[index].job),
+              ),
       ],
     );
   }
